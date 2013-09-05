cee221c44525d2eba4b6298b4b56262da9bb0dd7
XingKaiXin <XingKaiXin@XingKaiXin-X201>
2013-09-05 19:03:05 +0800

    2013-9-5

--- PYDemo/study/rssfetch.py
+++ PYDemo/study/rssfetch.py
@@ -3,10 +3,15 @@ import re
 
 
 def getRSS():
+  """
+  aa = getRSS()
+  for i in range(0,len(aa)/2):
+    print str(i)+":" + aa[i]['title']
+    print str(i)+":" +aa[i]['xmlUrl']
+  """
   f = open("feedly.opml")
   rss = f.read()
   abstarct = re.compile(r'<outline type="rss".*?"/>',re.DOTALL).findall(rss)
-  print abstarct
   vid_list = []
   for i in range(0,len(abstarct)):
         title = re.compile(r'title="(.*?)"',re.DOTALL).findall(abstarct[i])
@@ -18,6 +23,22 @@ def getRSS():
         vid_list.append(vid)  
   return vid_list
   
-aa = getRSS()
-print len(aa)/2
-print aa[0]['title']
+def getURL(url):
+    url = 'http://i.youku.com/u/UMjI4NTcyMDQ4/videos/'
+    res = urllib2.urlopen(url).read()
+    abstarct = re.compile(r'<ul class="v".*?</ul>',re.DOTALL).findall(res)
+    
+    vid_list = []
+    for i in range(0,len(abstarct)):
+        title = re.compile(r'title="(.*?)"',re.DOTALL).findall(abstarct[i])
+        href = re.compile(r'href="(.*?)"',re.DOTALL).findall(abstarct[i])
+        date = re.compile(r'<span>(.*?)</span>',re.DOTALL).findall(abstarct[i])
+        if debug == True:
+            print title[0]+href[0]+date[0]
+        vid = {
+            'title' : title[0],
+            'href'  : href[0],
+            'date'  : date[0]
+        }
+        vid_list.append(vid)  
+    return vid_list
